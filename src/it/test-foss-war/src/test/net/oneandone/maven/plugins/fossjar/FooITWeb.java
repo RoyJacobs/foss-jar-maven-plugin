c902ccee39c7daaffa06e3e04ecd5f88e9a8b8e3
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-07-04 22:51:06 +0200

    Further experiments with lifecycles.

---
+++ src/it/test-foss-war/src/test/net/oneandone/maven/plugins/fossjar/FooITWeb.java
@@ -0,0 +1,13 @@
+package net.oneandone.maven.plugins.fossjar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FooITWeb {
+
+    @Test
+    public void checkTomcat() {
+        int port = 8080;
+        assertTrue(false);
+    }
+}
