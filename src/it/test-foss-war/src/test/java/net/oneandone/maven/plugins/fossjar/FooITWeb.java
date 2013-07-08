09cc32f71241976ab772d9c4a58e364a8356bc51
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-07-08 21:08:02 +0200

    Include guava

--- src/it/test-foss-war/src/test/java/net/oneandone/maven/plugins/fossjar/FooITWeb.java
+++ src/it/test-foss-war/src/test/java/net/oneandone/maven/plugins/fossjar/FooITWeb.java
@@ -1,13 +1,25 @@
 package net.oneandone.maven.plugins.fossjar;
 
+import com.google.common.io.ByteStreams;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FooITWeb {
 
     @Test
-    public void checkTomcat() {
-        int port = 8080;
-        assertTrue(false);
+    public void checkTomcat() throws MalformedURLException, IOException {
+        int port = Integer.valueOf(System.getProperty("tomcat.port", "8080"));
+        URL url = new URL("http", "localhost", port, "/");
+        InputStream stream = url.openStream();
+        try {
+            final byte[] bytes = ByteStreams.toByteArray(stream);
+            System.err.println(new String(bytes));
+        } finally {
+            stream.close();
+        }
     }
 }
